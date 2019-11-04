bbc17628450c94a30ce12826dd3a3ad56c5342a7
Jahid Hassan <jahid.hassan@infolytx.com>
2019-11-04 12:21:15 +0600

    changed extension modifier

--- Source/Helpers/Extensions/UIColor+Extensions.swift
+++ Source/Helpers/Extensions/UIColor+Extensions.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 extension UIColor {
-    convenience init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1.0) {
+    public convenience init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1.0) {
         self.init(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: a)
     }
     

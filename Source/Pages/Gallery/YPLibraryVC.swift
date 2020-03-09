e6402a9a33724c6eefcb7965032ef601107a0e25
Jahid Hassan <jahid.hassan@infolytx.com>
2020-03-09 10:20:34 +0600

    fixed next button enable issue on default selection

--- Source/Pages/Gallery/YPLibraryVC.swift
+++ Source/Pages/Gallery/YPLibraryVC.swift
@@ -67,7 +67,7 @@ public class YPLibraryVC: UIViewController, YPPermissionCheckable {
         v.assetViewContainer.multipleSelectionButton.isHidden = !(YPConfig.library.maxNumberOfItems > 1)
         v.maxNumberWarningLabel.text = String(format: YPConfig.wordings.warningMaxItemsLimit, YPConfig.library.maxNumberOfItems)
         
-        if let preselectedItems = YPConfig.library.preselectedItems {
+        if let preselectedItems = YPConfig.library.preselectedItems, preselectedItems.count > 0 {
             selection = preselectedItems.compactMap { item -> YPLibrarySelection? in
                 var itemAsset: PHAsset?
                 switch item {
